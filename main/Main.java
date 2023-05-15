5a99a1230895e66a10f5282a911f5dfab06ffe5b
Tran Hung Dat <118071536+ThoSugoi@users.noreply.github.com>
2023-05-15 11:33:37 +0700

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,29 +1,29 @@
 package main;
 
 import java.awt.Color;
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
 import javax.swing.JFrame;
 
 public class Main {
+	
+	
 	public void startGame() {
-
-		public static void main(String[] args) {
-			JFrame frame = new JFrame();
-			frame.setSize(654, 678);
-			frame.setBackground(new Color(100, 100, 100));
-			//frame.getContentPane().setBackground(new Color(200, 200, 200));
-			//frame.setLayout(new GridBagLayout());
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setResizable(true);
-
-			Board board = new Board();
-			frame.add(board);
-			frame.setVisible(true);
-		}
+		JFrame frame = new JFrame();
+		frame.setSize(654, 678);
+		frame.setBackground(new Color(100, 100, 100));
+		//frame.getContentPane().setBackground(new Color(200, 200, 200));
+		//frame.setLayout(new GridBagLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
 		
+		Board board = new Board();
+		frame.add(board);
+		frame.setVisible(true);
+
 	}
-	public static void main(String[] args) {
-		Main chessGame = new Main();
-		chessGame.startGame();
+    public static void main(String[] args) {
+        Main chessGame = new Main();
+        chessGame.startGame();
 }}
-
-
