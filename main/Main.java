2181bbc9569a6e7ddd997077f75b4832c6ce531e
Tran Hung Dat <118071536+ThoSugoi@users.noreply.github.com>
2023-05-15 11:23:34 +0700

    Update Main.java
    add a method to the main class so it is more convinient to invoke

--- main/Main.java
+++ main/Main.java
@@ -4,20 +4,26 @@ import java.awt.Color;
 import javax.swing.JFrame;
 
 public class Main {
+	public void startGame() {
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setSize(654, 678);
-		frame.setBackground(new Color(100, 100, 100));
-		//frame.getContentPane().setBackground(new Color(200, 200, 200));
-		//frame.setLayout(new GridBagLayout());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(true);
+		public static void main(String[] args) {
+			JFrame frame = new JFrame();
+			frame.setSize(654, 678);
+			frame.setBackground(new Color(100, 100, 100));
+			//frame.getContentPane().setBackground(new Color(200, 200, 200));
+			//frame.setLayout(new GridBagLayout());
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setResizable(true);
+
+			Board board = new Board();
+			frame.add(board);
+			frame.setVisible(true);
+		}
 		
-		Board board = new Board();
-		frame.add(board);
-		frame.setVisible(true);
-
 	}
+	public static void main(String[] args) {
+		Main chessGame = new Main();
+		chessGame.startGame();
+}}
 
-}
+
