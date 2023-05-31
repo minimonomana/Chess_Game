75c6b53cf5f026c262978bbdd00096abcef24681
Spacmannn <132814811+Spacmannn@users.noreply.github.com>
2023-05-31 16:20:16 +0700

    Board.java

--- main/Board.java
+++ main/Board.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Stack;
 import java.util.Collections;
@@ -28,7 +29,10 @@ public class Board extends JPanel{
 	Players p1;
 	Players p2;
 	ChessTimerGUI timer;
-	
+	int mode;
+
+	int[] fakeBlack;
+	int[] fakeWhite;
 	
 	public Board(){
 		this.setPreferredSize(new Dimension(cols * tileSize, rows * tileSize));
@@ -36,13 +40,20 @@ public class Board extends JPanel{
 		this.addMouseListener(input);
 		this.addMouseMotionListener(input);
 		Scanner input = new Scanner(System.in);
-		int mode = input.nextInt();
+		mode = input.nextInt();
 		if (mode == 0) {
 			this.resetBoard();
 		}
 		else if (mode == 1) {
 			this.chess960Board();
 		}
+		else if (mode == 2){
+			this.fakeChess();
+		}
+		else if (mode == 3){
+			this.semiFakeChess();
+		}
+		Sound.playSound("C:\\Users\\nguye\\Downloads\\Git\\Chess_Game\\resources\\StartEndGame.wav");
 //		timer = new ChessTimerGUI();
 //		this.add(timer);
 		
@@ -86,7 +97,6 @@ public class Board extends JPanel{
 		pieceList.add(new King(this, 0, 4, false));
 		pieceList.add(new King(this, 7, 4, true));
 
-		
 	}
 	
 	public void chess960Board() {
@@ -136,6 +146,119 @@ public class Board extends JPanel{
 		pieceList.add(new King(this, 0, arr[4], false));
 		pieceList.add(new King(this, 7, arr[4], true));
 
+	}
+
+	public void fakeChess(){
+		fakeBlack = new int[15];
+		fakeWhite = new int[15];
+		List<Integer> list = new LinkedList<Integer>();
+		for (int i = 0; i < 15; i++) {
+			list.add(i);
+		}
+		Collections.shuffle(list);
+		for (int i = 0; i < 15; i++){
+			fakeBlack[i] = list.get(i);
+		}
+
+		Collections.shuffle(list);
+		for (int i = 0; i < 15; i++){
+			fakeWhite[i] = list.get(i);
+		}
+
+		// Adding Pawns
+		for(int i=0; i<8; i++) {
+		    pieceList.add(new Pawn(this, 1, i, false));
+		    pieceList.add(new Pawn(this, 6, i, true));
+		}
+
+		// Adding Rooks
+		pieceList.add(new Rook(this, 0, 0, false));
+		pieceList.add(new Rook(this, 0, 7, false));
+		pieceList.add(new Rook(this, 7, 0, true));
+		pieceList.add(new Rook(this, 7, 7, true));
+
+		// Adding Knights
+		pieceList.add(new Knight(this, 0, 1, false));
+		pieceList.add(new Knight(this, 0, 6, false));
+		pieceList.add(new Knight(this, 7, 1, true));
+		pieceList.add(new Knight(this, 7, 6, true));
+
+		// Adding Bishops
+		pieceList.add(new Bishop(this, 0, 2, false));
+		pieceList.add(new Bishop(this, 0, 5, false));
+		pieceList.add(new Bishop(this, 7, 2, true));
+		pieceList.add(new Bishop(this, 7, 5, true));
+
+		// Adding Queens
+		pieceList.add(new Queen(this, 0, 3, false));
+		pieceList.add(new Queen(this, 7, 3, true));
+
+		for (Piece piece: pieceList){
+			piece.sprite = piece.fakesheet.getSubimage(piece.iswhite ? 0: piece.fakesheetscale, 0, piece.fakesheetscale, piece.fakesheetscale).getScaledInstance(tileSize, tileSize, BufferedImage.SCALE_SMOOTH);
+		}
+		// Adding Kings
+		pieceList.add(new King(this, 0, 4, false));
+		pieceList.add(new King(this, 7, 4, true));
+
+	}
+
+	public void semiFakeChess(){
+		fakeBlack = new int[15];
+		fakeWhite = new int[15];
+		List<Integer> list = new LinkedList<Integer>();
+		for (int i = 0; i < 15; i++) {
+			list.add(i);
+		}
+		Collections.shuffle(list);
+		for (int i = 0; i < 15; i++){
+			fakeBlack[i] = list.get(i);
+		}
+
+		Collections.shuffle(list);
+		for (int i = 0; i < 15; i++){
+			fakeWhite[i] = list.get(i);
+		}
+
+		// Adding Rooks
+		pieceList.add(new Rook(this, 0, 0, false));
+		pieceList.add(new Rook(this, 0, 7, false));
+		pieceList.add(new Rook(this, 7, 0, true));
+		pieceList.add(new Rook(this, 7, 7, true));
+
+		// Adding Knights
+		pieceList.add(new Knight(this, 0, 1, false));
+		pieceList.add(new Knight(this, 0, 6, false));
+		pieceList.add(new Knight(this, 7, 1, true));
+		pieceList.add(new Knight(this, 7, 6, true));
+
+		// Adding Bishops
+		pieceList.add(new Bishop(this, 0, 2, false));
+		pieceList.add(new Bishop(this, 0, 5, false));
+		pieceList.add(new Bishop(this, 7, 2, true));
+		pieceList.add(new Bishop(this, 7, 5, true));
+
+		// Adding Queens
+		pieceList.add(new Queen(this, 0, 3, false));
+		pieceList.add(new Queen(this, 7, 3, true));
+
+		for (Piece piece: pieceList){
+			piece.sprite = piece.fakesheet.getSubimage(piece.iswhite ? 0: piece.fakesheetscale, 0, piece.fakesheetscale, piece.fakesheetscale).getScaledInstance(tileSize, tileSize, BufferedImage.SCALE_SMOOTH);
+		}
+
+		// Adding Pawns
+		for(int i=0; i<8; i++) {
+		    pieceList.add(new Pawn(this, 1, i, false));
+		    pieceList.add(new Pawn(this, 6, i, true));
+		}
+		for (Piece piece: pieceList){
+			if (piece.name.equals("Pawn")){
+				piece.hasFakeMoved = true;
+			}
+		}
+
+		// Adding Kings
+		pieceList.add(new King(this, 0, 4, false));
+		pieceList.add(new King(this, 7, 4, true));
 
 	}
 	
@@ -154,21 +277,28 @@ public class Board extends JPanel{
 		return null;
 	}
 	
-	Stack<Move> moveList = new Stack<Move>();
+	public Stack<Move> moveList = new Stack<Move>();
 	
 	public void move(Move m) {
+		if (m.capture != null){
+			Sound.playSound("C:\\Users\\nguye\\Downloads\\Git\\Chess_Game\\resources\\Capture.wav");
+		}
+		else{
+			Sound.playSound("C:\\Users\\nguye\\Downloads\\Git\\Chess_Game\\resources\\Move.wav");
+		}
+		
 		if (m.piece.name.equals("King")) {
 			castle(m);
 		}
 		else if (m.piece.name.equals("Pawn") && Math.abs(m.newrow - m.oldrow) == 1 && Math.abs(m.newcol - m.oldcol) == 1) {
 			enPassant(m);
 		}
+
 		m.piece.col = m.newcol;
 		m.piece.row = m.newrow;
 		m.piece.xpos = m.newcol * tileSize;
 		m.piece.ypos = m.newrow * tileSize;
 		m.piece.hasMoved = true;
-
 
 		capture(m);
 
@@ -180,12 +310,15 @@ public class Board extends JPanel{
 		else if (p2.turn() == true){
 			p2.nextTurn(false);
 			p1.nextTurn(true);
-			
+		}
+		if (isCheckMated(p1.turn())) {
+			System.out.println("CHECKMATE!!!");
 		}
 		
 	}
 	
 	public void undo() {
+		Sound.playSound("C:\\Users\\nguye\\Downloads\\Git\\Chess_Game\\resources\\Move.wav");
 		Move m = moveList.pop();
 		if (m.piece.name.equals("King")) {
 			if (Math.abs(m.newcol - m.oldcol) > 1) {
@@ -216,6 +349,7 @@ public class Board extends JPanel{
 			m.capture.xpos = m.newcol * tileSize;
 			m.capture.ypos = m.newrow * tileSize;
 		}
+		m.piece.hasMoved = false;
 		p1.nextTurn(!p1.turn());
 		p2.nextTurn(!p2.turn());
 		
@@ -241,6 +375,51 @@ public class Board extends JPanel{
 			rook.xpos = rook.col * tileSize;
 			rook.ypos = rook.row * tileSize;
 			rook.hasMoved = true;
+
+			pieceList.remove(rook);
+			Piece fakePiece = null;
+			if (m.piece.iswhite){
+				int newPiece = fakeWhite[m.piece.col < m.newcol ? 8: 14];
+				if (newPiece < 8){
+					fakePiece = new Pawn(this, rook.row, rook.col, m.piece.iswhite);
+				}
+				else{
+					if (newPiece == 8 || newPiece == 14){
+						fakePiece = new Rook(this, rook.row, rook.col, m.piece.iswhite);
+					}
+					else if(newPiece == 9 || newPiece == 13){
+						fakePiece = new Knight(this, rook.row, rook.col, m.piece.iswhite);
+					}
+					else if (newPiece == 10 || newPiece == 12){
+						fakePiece = new Bishop(this, rook.row, rook.col, m.piece.iswhite);
+					}
+					else if (newPiece == 11){
+						fakePiece = new Queen(this, rook.row, rook.col, m.piece.iswhite);
+					}
+				}
+			}
+			else{
+				int newPiece = fakeBlack[m.piece.col < m.newcol ? 8: 14];
+				if (newPiece < 8){
+					fakePiece = new Pawn(this, rook.row, rook.col, m.piece.iswhite);
+				}
+				else{
+					if (newPiece == 8 || newPiece == 14){
+						fakePiece = new Rook(this, rook.row, rook.col, m.piece.iswhite);
+					}
+					else if(newPiece == 9 || newPiece == 13){
+						fakePiece = new Knight(this, rook.row, rook.col, m.piece.iswhite);
+					}
+					else if (newPiece == 10 || newPiece == 12){
+						fakePiece = new Bishop(this, rook.row, rook.col, m.piece.iswhite);
+					}
+					else if (newPiece == 11){
+						fakePiece = new Queen(this, rook.row, rook.col, m.piece.iswhite);
+					}
+				}
+			}
+			pieceList.add(fakePiece);
+			fakePiece.hasFakeMoved = true;
 		}
 		
 	}
@@ -249,36 +428,153 @@ public class Board extends JPanel{
 		if (!moveList.isEmpty()) {
 			Move lastMove = moveList.peek();
 			Piece lastPiece = moveList.peek().piece;
+			System.out.println(p1.turn());
 			if (p1.turn() && m.oldrow == 3 && m.newrow == 2 && lastPiece.name.equals("Pawn") &&
 					lastMove.newrow == 3 && lastMove.oldrow == 1 && m.newcol == lastMove.newcol && Math.abs(m.newcol - m.oldcol) == 1) {
 				pieceList.remove(get(3, m.newcol));
 			}
-			else if (p1.turn() && m.oldrow == 4 && m.newrow == 5 && lastPiece.name.equals("Pawn") &&
-					lastMove.newrow == 4 && lastMove.oldrow == 6 && m.newcol == lastMove.newcol && Math.abs(m.newcol - m.oldcol) == 6) {
+			else if (p2.turn() && m.oldrow == 4 && m.newrow == 5 && lastPiece.name.equals("Pawn") &&
+					lastMove.newrow == 4 && lastMove.oldrow == 6 && m.newcol == lastMove.newcol && Math.abs(m.newcol - m.oldcol) == 1) {
 				pieceList.remove(get(4, m.newcol));
 			}
 		}
 	}
 	
 	public boolean isValidMove(Move move) {
-
-		if (p1.turn() == move.piece.iswhite) {
-			
-			if (move.piece.canMove(move.newrow, move.newcol)) {
-				System.out.printf("%s %s can move from %d %d to %d %d\n", move.piece.iswhite ? "white" : "black", move.piece.name, move.piece.row, move.piece.col, move.newrow, move.newcol);
+		int rowKing = 0;
+		int colKing = 0;
+		boolean isWhiteKing = move.piece.iswhite;
+		for (Piece piece : pieceList) {
+			if (piece.iswhite == move.piece.iswhite && piece.name.equals("King")) {
+				rowKing = piece.row;
+				colKing = piece.col;
+				break;
+			}
+		}
+		if (p1.turn() == move.piece.iswhite && move.piece.canMove(move.newrow, move.newcol)) {
+			if (move.piece.name.equals("King")) {
+				if (checkAttacked(move.newrow, move.newcol, isWhiteKing)) {
+					return false;
+				}
 				return true;
 			}
-			
+			boolean isPinned = false;
+			int rowIncrement = 0;
+			int colIncrement = 0;
+			if (move.piece.row > rowKing) {
+				rowIncrement = 1;
+			}
+			else if (move.piece.row < rowKing) {
+				rowIncrement = -1;
+			}
+			if (move.piece.col > colKing) {
+				colIncrement = 1;
+			}
+			else if (move.piece.col < colKing) {
+				colIncrement = -1;
+			}
+			int curRow = rowKing;
+			int curCol = colKing;
+			boolean wasPiece = false;
+			boolean isMoveInPinLine = false;
+			while (curRow > -1 && curRow < 8 && curCol > -1 && curCol < 8) {
+				curRow += rowIncrement;
+				curCol += colIncrement;
+				if (move.newrow == curRow && move.newcol == curCol) {
+					isMoveInPinLine = true;
+				}
+				if (wasPiece && get(curRow, curCol) != null) {
+					Piece pinner = get(curRow, curCol);
+					if (pinner.iswhite == p1.turn()) {
+						break;
+					}
+					if ((pinner.name.equals("Bishop") || pinner.name.equals("Queen")) && Math.abs(rowIncrement) + Math.abs(colIncrement) == 2) {
+						isPinned = true;
+						break;
+					}
+					else if ((pinner.name.equals("Rook") || pinner.name.equals("Queen")) && Math.abs(rowIncrement) + Math.abs(colIncrement) == 1) {
+						isPinned = true;
+						break;
+					}
+				}
+				else if (!wasPiece && get(curRow, curCol) != null) {
+					if (get(curRow, curCol) == move.piece) {
+						wasPiece = true;
+					}
+					else {
+						break;
+					}
+				}
+			}
 
+			if (!checkAttacked(rowKing, colKing, isWhiteKing)) {
+				if (isPinned) {
+					if (isMoveInPinLine) {
+						return true;
+					}
+					return false;
+				}
+				return true;
+			}
+			int numOfChecks = 0;
+			Piece curAttacker = move.piece;
+			for (Piece attacker : pieceList) {
+				if (attacker.canAttack(rowKing, colKing) && attacker.iswhite != isWhiteKing) {
+					numOfChecks += 1;
+					curAttacker = attacker;
+				}
+			}
+			if (numOfChecks > 1) {
+				return false;
+			}
+			if (move.newrow == curAttacker.row && move.newcol == curAttacker.col && !move.piece.name.equals("King")) {
+				return true;
+			}
+			if (curAttacker.name.equals("Bishop") || curAttacker.name.equals("Rook") || curAttacker.name.equals("Queen")) {
+				boolean canBlock = false;
+				if (move.newrow <= Math.max(curAttacker.row, rowKing) && move.newrow >= Math.min(curAttacker.row, rowKing)
+						&& move.newcol <= Math.max(curAttacker.col, colKing) && move.newcol >= Math.min(curAttacker.col, rowKing)) {
+					rowIncrement = 0;
+					colIncrement = 0;
+					if (curAttacker.row > rowKing) {
+						rowIncrement = 1;
+					} else if (curAttacker.row < rowKing) {
+						rowIncrement = -1;
+					}
+					if (curAttacker.col > colKing) {
+						colIncrement = 1;
+					} else if (curAttacker.col < colKing) {
+						colIncrement = -1;
+					}
+					curRow = rowKing;
+					curCol = colKing;
+					while (curRow != curAttacker.row && curCol != curAttacker.col) {
+						curRow += rowIncrement;
+						curCol += colIncrement;
+						if (move.newrow == curRow && move.newcol == curCol) {
+							canBlock = true;
+							break;
+						}
+					}
+				}
+				if (canBlock) {
+					if (isPinned && !isMoveInPinLine) {
+						return false;
+					}
+					return true;
+				}
+				return false;
+			}
+			return true;
+			// System.out.printf("%s %s can move from %d %d to %d %d\n", move.piece.iswhite ? "white" : "black", move.piece.name, move.piece.row, move.piece.col, move.newrow, move.newcol);
 		}
-		
 		return false;
 	}
 	
 	public boolean checkAttacked(int row, int col, boolean white) {
 		
 		for (Piece piece: pieceList) {
-			System.out.println(piece.name);
+			// System.out.println(piece.name);
 			if (piece.iswhite != white) {
 				if (piece.canAttack(row, col)) {
 					System.out.printf("%d %d attacks %d %d\n", piece.row, piece.col, row, col);
@@ -289,8 +585,26 @@ public class Board extends JPanel{
 		}
 		
 		return false;
-		
-		
+	}
+
+	public boolean isCheckMated(boolean isWhite) {
+		for (Piece piece : pieceList) {
+			if (piece.iswhite == isWhite) {
+				if (piece.name.equals("King")) {
+					if (!checkAttacked(piece.row, piece.col, isWhite)) {
+						return false;
+					}
+				}
+				for (int i = 0; i < 8; i++) {
+					for (int j = 0; j < 8; j++) {
+						if (isValidMove(new Move(this, piece, i, j))) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -317,7 +631,7 @@ public class Board extends JPanel{
 					}
 				}
 			}
-			System.out.println();
+			// System.out.println();
 		}
 		
 		for (Piece piece: pieceList) {
